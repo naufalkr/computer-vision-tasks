56de40acfd8a6a5baae6e8160cd420fd4965421d
Naufal Khairul <naufalkr394@gmail.com>
2024-09-16 11:16:47 +0700

    added opencv filters

--- script.js
+++ script.js
@@ -11,10 +11,12 @@ const sobelButton = document.getElementById('sobel');
 const bwButton = document.getElementById('bw');
 const brightnessButton = document.getElementById('brightness');
 const glpfButton = document.getElementById('glpf');
-const sharpenButton = document.getElementById('sharpen');
+const bilateralButton = document.getElementById('bilateral').addEventListener('click', applyBilateral);
 // const fftButton = document.getElementById('fft');
 // const fftButton = document.getElementById('homomorphic').addEventListener('click', applyHomomorphicFilter);
+const laplacianButton = document.getElementById('laplacian').addEventListener('click', applyLaplacian);
 const claheButton = document.getElementById('clahe').addEventListener('click', applyCLAHE);
+const dftButton = document.getElementById('dft').addEventListener('click', applyDFT);
 
 
 let currentImageSource = null;
@@ -178,51 +180,28 @@ brightnessButton.addEventListener('click', () => {
     filteredImage.style.display = 'block';
 });
 
-
-// sharpening filter
-sharpenButton.addEventListener('click', () => {
-    const context = canvas.getContext('2d');
-    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data;
-
-    const kernel = [
-        [0, -1, 0],
-        [-1, 5, -1],
-        [0, -1, 0]
-    ];
-
-    function applyKernel(x, y) {
-        let r = 0, g = 0, b = 0;
-        for (let ky = -1; ky <= 1; ky++) {
-            for (let kx = -1; kx <= 1; kx++) {
-                const px = Math.min(canvas.width - 1, Math.max(0, x + kx));
-                const py = Math.min(canvas.height - 1, Math.max(0, y + ky));
-                const idx = (py * canvas.width + px) * 4;
-                r += data[idx] * kernel[ky + 1][kx + 1];
-                g += data[idx + 1] * kernel[ky + 1][kx + 1];
-                b += data[idx + 2] * kernel[ky + 1][kx + 1];
-            }
-        }
-        return [r, g, b];
+// Laplacian
+function applyLaplacian() {
+    if (typeof cv === 'undefined') {
+        alert("OpenCV.js not loaded!");
+        return;
     }
 
-    const outputData = context.createImageData(canvas.width, canvas.height);
+    const src = cv.imread('canvas');  
+    let dst = new cv.Mat();
+    cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
 
-    for (let y = 0; y < canvas.height; y++) {
-        for (let x = 0; x < canvas.width; x++) {
-            const idx = (y * canvas.width + x) * 4;
-            const [r, g, b] = applyKernel(x, y);
-            outputData.data[idx] = Math.min(255, Math.max(0, r));
-            outputData.data[idx + 1] = Math.min(255, Math.max(0, g));
-            outputData.data[idx + 2] = Math.min(255, Math.max(0, b));
-            outputData.data[idx + 3] = 255;
-        }
-    }
+    cv.Laplacian(src, dst, cv.CV_8U, 1, 1, 0, cv.BORDER_DEFAULT);
+    cv.imshow('canvas', dst);
 
-    context.putImageData(outputData, 0, 0);
+    const canvas = document.getElementById('canvas');
+    const filteredImage = document.getElementById('filteredImage'); 
+
     filteredImage.src = canvas.toDataURL('image/png');
-    filteredImage.style.display = 'block';
-});
+    filteredImage.style.display = 'block'; 
+    
+    src.delete(); dst.delete();
+}
 
 // CLAHE
 function applyCLAHE() {
@@ -251,6 +230,118 @@ function applyCLAHE() {
     gray.delete();
     dst.delete();
     clahe.delete();
+}
+
+
+// CLAHE
+function applyBilateral() {
+    if (typeof cv === 'undefined') {
+        alert("OpenCV.js not loaded!");
+        return;
+    }
+
+    const src = cv.imread('canvas');  
+    let dst = new cv.Mat();
+    cv.cvtColor(src, src, cv.COLOR_RGBA2RGB, 0);
+
+    // You can try more different parameters
+    cv.bilateralFilter(src, dst, 9, 75, 75, cv.BORDER_DEFAULT);
+    cv.imshow('canvas', dst);
+
+    const canvas = document.getElementById('canvas');
+    const filteredImage = document.getElementById('filteredImage'); 
+
+    filteredImage.src = canvas.toDataURL('image/png');
+    filteredImage.style.display = 'block'; 
+
+    src.delete(); dst.delete();
+}
+
+
+
+// DFT
+function applyDFT() {
+    if (typeof cv === 'undefined') {
+        alert("OpenCV.js not loaded!");
+        return;
+    }
+
+    const src = cv.imread('canvas');  
+    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+
+    
+    // get optimal size of DFT
+    let optimalRows = cv.getOptimalDFTSize(src.rows);
+    let optimalCols = cv.getOptimalDFTSize(src.cols);
+    let s0 = cv.Scalar.all(0);
+    let padded = new cv.Mat();
+    cv.copyMakeBorder(src, padded, 0, optimalRows - src.rows, 0,
+                    optimalCols - src.cols, cv.BORDER_CONSTANT, s0);
+
+    // use cv.MatVector to distribute space for real part and imaginary part
+    let plane0 = new cv.Mat();
+    padded.convertTo(plane0, cv.CV_32F);
+    let planes = new cv.MatVector();
+    let complexI = new cv.Mat();
+    let plane1 = new cv.Mat.zeros(padded.rows, padded.cols, cv.CV_32F);
+    planes.push_back(plane0);
+    planes.push_back(plane1);
+    cv.merge(planes, complexI);
+
+    // in-place dft transform
+    cv.dft(complexI, complexI);
+
+    // compute log(1 + sqrt(Re(DFT(img))**2 + Im(DFT(img))**2))
+    cv.split(complexI, planes);
+    cv.magnitude(planes.get(0), planes.get(1), planes.get(0));
+    let mag = planes.get(0);
+    let m1 = new cv.Mat.ones(mag.rows, mag.cols, mag.type());
+    cv.add(mag, m1, mag);
+    cv.log(mag, mag);
+
+    // crop the spectrum, if it has an odd number of rows or columns
+    let rect = new cv.Rect(0, 0, mag.cols & -2, mag.rows & -2);
+    mag = mag.roi(rect);
+
+    // rearrange the quadrants of Fourier image
+    // so that the origin is at the image center
+    let cx = mag.cols / 2;
+    let cy = mag.rows / 2;
+    let tmp = new cv.Mat();
+
+    let rect0 = new cv.Rect(0, 0, cx, cy);
+    let rect1 = new cv.Rect(cx, 0, cx, cy);
+    let rect2 = new cv.Rect(0, cy, cx, cy);
+    let rect3 = new cv.Rect(cx, cy, cx, cy);
+
+    let q0 = mag.roi(rect0);
+    let q1 = mag.roi(rect1);
+    let q2 = mag.roi(rect2);
+    let q3 = mag.roi(rect3);
+
+    // exchange 1 and 4 quadrants
+    q0.copyTo(tmp);
+    q3.copyTo(q0);
+    tmp.copyTo(q3);
+
+    // exchange 2 and 3 quadrants
+    q1.copyTo(tmp);
+    q2.copyTo(q1);
+    tmp.copyTo(q2);
+
+    // The pixel value of cv.CV_32S type image ranges from 0 to 1.
+    cv.normalize(mag, mag, 0, 1, cv.NORM_MINMAX);
+
+    // cv.imshow('canvasOutput', mag);
+
+    cv.imshow('canvas', mag);
+
+    const canvas = document.getElementById('canvas');
+    const filteredImage = document.getElementById('filteredImage'); 
+
+    filteredImage.src = canvas.toDataURL('image/png');
+    filteredImage.style.display = 'block'; 
+    src.delete(); padded.delete(); planes.delete(); complexI.delete(); m1.delete(); tmp.delete();
 }
 
 // Gaussian low pass filter
